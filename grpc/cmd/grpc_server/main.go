76e4d36c14bae0ecd294c9b4b9be49b81f4fbb93
Ippolid <iliamishin2006@gmail.com>
2025-04-03 15:57:49 +0300

    edit server and other

--- grpc/cmd/grpc_server/main.go
+++ grpc/cmd/grpc_server/main.go
@@ -6,58 +6,41 @@ import (
 	"log"
 	"net"
 
-	"github.com/Ippolid/auth/grpc/pkg/chatserver_v1"
+	"github.com/Ippolid/chat-server/grpc/pkg/chatserver_v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/brianvoe/gofakeit"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 const grpcPort = 50051
 
 type server struct {
-	auth_v1.UnimplementedAuthV1Server
+	chatserver_v1.UnimplementedAuthV1Server
 }
 
 // Get ...
-func (s *server) Get(_ context.Context, req *auth_v1.GetRequest) (*auth_v1.GetResponse, error) {
-	log.Printf("Note id: %d", req.GetId())
 
-	return &auth_v1.GetResponse{
-		Note: &auth_v1.UserGet{
-			Id: req.GetId(),
-			Info: &auth_v1.UserInfo{
-				Name:  gofakeit.BeerName(),
-				Email: gofakeit.Email(),
-			},
-			CreatedAt: timestamppb.New(gofakeit.Date()),
-			UpdatedAt: timestamppb.New(gofakeit.Date()),
-		},
-	}, nil
-}
-
-func (s *server) Create(_ context.Context, req *auth_v1.CreateRequest) (*auth_v1.CreateResponse, error) {
+func (s *server) Create(_ context.Context, req *chatserver_v1.CreateRequest) (*chatserver_v1.CreateResponse, error) {
 	//чето делается
-	fmt.Printf("name +%v\n", req.Info)
+	fmt.Printf("name +%v\n", req.Usernames)
 
-	return &auth_v1.CreateResponse{
+	return &chatserver_v1.CreateResponse{
 		Id: gofakeit.Int64(),
 	}, nil
 }
 
-func (s *server) Update(_ context.Context, req *auth_v1.UpdateRequest) (*emptypb.Empty, error) {
+func (s *server) Delete(_ context.Context, req *chatserver_v1.DeleteRequest) (*emptypb.Empty, error) {
 	//чето делается
 	fmt.Printf("User id: %d", req.GetId())
-	fmt.Printf("name +%v\n", req.Info)
 
 	return &emptypb.Empty{}, nil
 }
 
-func (s *server) Delete(_ context.Context, req *auth_v1.DeleteRequest) (*emptypb.Empty, error) {
+func (s *server) SendMessage(_ context.Context, req *chatserver_v1.SendMessageRequest) (*emptypb.Empty, error) {
 	//чето делается
-	fmt.Printf("User id: %d", req.GetId())
+	fmt.Printf("User id: %d", req.Message, req.Timestamp)
 
 	return &emptypb.Empty{}, nil
 }
@@ -70,7 +53,7 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	auth_v1.RegisterAuthV1Server(s, &server{})
+	chatserver_v1.RegisterAuthV1Server(s, &server{})
 
 	log.Printf("server listening at %v", lis.Addr())
 
